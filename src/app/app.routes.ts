641c9506865cacec53ba2b375146065e45f92c93
AGVVsivakiran <sivakiran.adari@gmail.com>
2025-08-24 15:13:13 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Error } from './components/error/error';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: Login,
+    },
+    {
+        path:'dashboard', component: Dashboard
+    },
+    {
+        path:'**', component: Error,
+    },
+];
